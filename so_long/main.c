ddb39ed56ecac30bd9d7194d0ebc874420af6a64
Andre Almeida <andre_rdo@hotmail.com>
2023-01-10 20:44:20 -0300

    Initial Commit

---
+++ so_long/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  so_long
+//
+//  Created by Andre Almeida on 10/01/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
