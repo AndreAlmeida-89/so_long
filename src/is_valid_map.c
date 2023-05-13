ad2471d11d08fb2b3bea5b50da54fd0317081d59
André Almeida <andre_rdo@hotmail.com>
2023-05-13 10:40:51 -0300

    free map at ins_valid_map

--- src/is_valid_map.c
+++ src/is_valid_map.c
@@ -6,7 +6,7 @@
 /*   By: andde-so <andde-so@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 10:39:35 by andde-so          #+#    #+#             */
-/*   Updated: 2023/02/18 18:10:52 by andde-so         ###   ########.fr       */
+/*   Updated: 2023/05/13 10:34:55 by andde-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 int	is_valid_map(char **map)
 {
 	if (!is_rectangle(map))
-		return (print_error("The map must be rectangular."));
+		return (print_error("The map must be rectangular.", map));
 	if (!contains_only(map, (t_content *)"01PCE"))
-		return (print_error("The map has invalid characters"));
+		return (print_error("The map has invalid characters", map));
 	if (!has_valid_number_of_objects(map))
-		return (print_error("The map has invalid number of objects."));
+		return (print_error("The map has invalid number of objects.", map));
 	if (!is_surrounded_by_walls(map))
-		return (print_error("The map must be closed/surrounded by walls."));
+		return (print_error("The map must be surrounded by walls.", map));
 	if (!is_playable(map))
-		return (print_error("There's no valid path in the map."));
+		return (print_error("There's no valid path in the map.", map));
 	return (1);
 }
