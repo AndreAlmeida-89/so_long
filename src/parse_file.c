64fa635801231bd9a3648b90037849d997608de0
André Almeida <andre_rdo@hotmail.com>
2023-05-20 11:45:37 -0300

    fix parse file

--- src/parse_file.c
+++ src/parse_file.c
@@ -6,12 +6,11 @@
 /*   By: andde-so <andde-so@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 22:18:19 by andde-so          #+#    #+#             */
-/*   Updated: 2023/05/18 08:43:34 by andde-so         ###   ########.fr       */
+/*   Updated: 2023/05/20 11:39:32 by andde-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-
 
 static int	check_extention(const char *path, const char *extension)
 {
@@ -25,49 +24,30 @@ static int	check_extention(const char *path, const char *extension)
 	return (ft_strncmp(dot + 1, extension, ft_strlen(dot + 1)) == 0);
 }
 
-static void	*ft_realloc(void *ptr, size_t size)
-{
-	void	*new_ptr;
-
-	if (ptr == NULL)
-		return (malloc(size));
-	if (size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	new_ptr = malloc(size);
-	if (new_ptr == NULL)
-		return (NULL);
-	ft_memcpy(new_ptr, ptr, size);
-	free(ptr);
-	return (new_ptr);
-}
-
 static char	*read_file(const char *path)
 {
-	int		fd;
-	char	buffer[BUFF_SIZE];
-	char	*result;
-	size_t	result_len;
-	ssize_t	bytes_read;
+	int			fd;
+	char		buffer[BUFF_SIZE + 1];
+	char		*result;
+	char		*temp;
+	ssize_t		bytes;
 
 	fd = open(path, O_RDONLY);
 	if (fd == -1)
+		print_error("Path can not be open.", NULL);
+	result = ft_strdup("");
+	while (1)
 	{
-		close(fd);
-		print_error("File can not be read.", NULL);
+		bytes = read(fd, buffer, BUFF_SIZE);
+		if (bytes == -1 && close(fd) == 0)
+			print_error("File can not be read.", NULL);
+		if (bytes == 0)
+			break ;
+		buffer[bytes] = '\0';
+		temp = ft_strjoin(result, buffer);
+		free(result);
+		result = temp;
 	}
-	result = NULL;
-	result_len = 0;
-	while ((bytes_read = read(fd, buffer, sizeof(buffer))) > 0)
-	{
-		result = ft_realloc(result, result_len + bytes_read + 1);
-		ft_memcpy(result + result_len, buffer, bytes_read);
-		result_len += bytes_read;
-	}
-	if (result != NULL)
-		result[result_len] = '\0';
 	close(fd);
 	return (result);
 }
@@ -81,8 +61,7 @@ char	**parse_file(const char *path)
 		print_error("Invalid file extension.", NULL);
 	file_content = read_file(path);
 	map = ft_split(file_content, '\n');
-	if (!map)
-		print_error("File has no content.", NULL);
+	free(file_content);
 	if (!*map)
 		print_error("Empty file.", map);
 	return (map);
--- src_bonus/parse_file_bonus.c
+++ src_bonus/parse_file_bonus.c
@@ -6,7 +6,7 @@
 /*   By: andde-so <andde-so@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 22:18:19 by andde-so          #+#    #+#             */
-/*   Updated: 2023/05/15 11:26:09 by andde-so         ###   ########.fr       */
+/*   Updated: 2023/05/20 11:43:50 by andde-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,24 +24,45 @@ static int	check_extention(const char *path, const char *extension)
 	return (ft_strncmp(dot + 1, extension, ft_strlen(dot + 1)) == 0);
 }
 
+static char	*read_file(const char *path)
+{
+	int			fd;
+	char		buffer[BUFF_SIZE + 1];
+	char		*result;
+	char		*temp;
+	ssize_t		bytes;
+
+	fd = open(path, O_RDONLY);
+	if (fd == -1)
+		print_error("Path can not be open.", NULL);
+	result = ft_strdup("");
+	while (1)
+	{
+		bytes = read(fd, buffer, BUFF_SIZE);
+		if (bytes == -1 && close(fd) == 0)
+			print_error("File can not be read.", NULL);
+		if (bytes == 0)
+			break ;
+		buffer[bytes] = '\0';
+		temp = ft_strjoin(result, buffer);
+		free(result);
+		result = temp;
+	}
+	close(fd);
+	return (result);
+}
+
 char	**parse_file(const char *path)
 {
 	char	**map;
-	char	buffer[BUFF_SIZE];
-	int		fd;
+	char	*file_content;
 
 	if (!check_extention(path, "ber"))
 		print_error("Invalid file extension.", NULL);
-	ft_bzero(buffer, BUFF_SIZE);
-	fd = open(path, O_RDONLY);
-	if (fd < 0 || read(fd, buffer, BUFF_SIZE) < 0)
-	{
-		print_error("File can not be read.", NULL);
-		close(fd);
-	}
-	map = ft_split(buffer, '\n');
+	file_content = read_file(path);
+	map = ft_split(file_content, '\n');
+	free(file_content);
 	if (!*map)
 		print_error("Empty file.", map);
-	close(fd);
 	return (map);
 }
