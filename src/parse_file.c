41fb33482dec55687a575714e8b3746c0e148a20
André Almeida <andre_rdo@hotmail.com>
2023-05-23 17:58:31 -0300

    fix check_extension function

--- src/parse_file.c
+++ src/parse_file.c
@@ -6,7 +6,7 @@
 /*   By: andde-so <andde-so@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 22:18:19 by andde-so          #+#    #+#             */
-/*   Updated: 2023/05/20 11:39:32 by andde-so         ###   ########.fr       */
+/*   Updated: 2023/05/23 17:54:26 by andde-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,21 @@
 
 static int	check_extention(const char *path, const char *extension)
 {
-	char	*dot;
+	size_t	i;
+	size_t	j;
 
-	if (!path || ft_strlen(path) <= ft_strlen(extension) + 1)
-		return (0);
-	dot = ft_strrchr(path, '.');
-	if (!dot)
-		return (0);
-	return (ft_strncmp(dot + 1, extension, ft_strlen(dot + 1)) == 0);
+	i = ft_strlen(path);
+	j = ft_strlen(extension);
+	while (path[--i] && extension[--j])
+		if (path[i] != extension[j])
+			return (0);
+	return (1);
+}
+
+void	clean_str(char **tab)
+{
+	while (*tab)
+		*tab = ft_strtrim(*tab++, "\r");
 }
 
 static char	*read_file(const char *path)
@@ -57,10 +64,11 @@ char	**parse_file(const char *path)
 	char	**map;
 	char	*file_content;
 
-	if (!check_extention(path, "ber"))
+	if (!check_extention(path, ".ber"))
 		print_error("Invalid file extension.", NULL);
 	file_content = read_file(path);
 	map = ft_split(file_content, '\n');
+	clean_str(map);
 	free(file_content);
 	if (!*map)
 		print_error("Empty file.", map);
